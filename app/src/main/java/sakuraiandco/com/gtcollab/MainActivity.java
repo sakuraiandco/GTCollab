fe749542ca5e8b260111c883c5bc91a1911babf5
kaliqwang <kaliqwang@yahoo.com>
2017-10-10 23:19:08 -0400

    fixed main package pathname

--- app/src/main/java/sakuraiandco/com/gtcollab/MainActivity.java
+++ app/src/main/java/sakuraiandco/com/gtcollab/MainActivity.java
@@ -1,15 +1,9 @@
-package sakuraiandco.com.gtcollab.activity;
+package sakuraiandco.com.gtcollab;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
-
-import sakuraiandco.com.gtcollab.R;
 
 public class MainActivity extends AppCompatActivity {
 
